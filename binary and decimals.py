ae67278108ba81418b32f6a891e6b6bde28b81df
Liza129 <144443800+Liza129@users.noreply.github.com>
2024-01-26 12:36:28 +0000

    Update binary and decimals.py

--- binary and decimals.py
+++ binary and decimals.py
@@ -1,7 +1,7 @@
 import functions_binary_and_dicimals
 
-user_ans = int(input("if you want to convert a binary number to a decimal, enter 1. \nDecimal to a binary, enter 2. \nAdd two binary numbers, enter 3. \nAnd if you want to finish, enter 4 \n"))
-while user_ans != 4:
+user_ans = int(input("if you want to convert a binary number to a decimal, enter 1. \nDecimal to a binary, enter 2. \nAdd two binary numbers, enter 3. \nHexadecimals to decimals, enter 4. \n Decimals to hexadecimals, enter 5. \nHexadecimals to binary, enter 6. \nBinary to hexadecimals, enter 7.\n And if you want to finish, enter 8. \n "))
+while user_ans != 8:
     if user_ans == 1:
         b_0 = input("Enter a binary number: ")
         sum_n = functions_binary_and_dicimals.biToDecimal(b_0)
@@ -15,7 +15,11 @@ while user_ans != 4:
         b2 = input("Enter second binary number: ")
         sum_bi = functions_binary_and_dicimals.addToBi(b1,b2)
         print("The sum of binary numbers will be: ", sum_bi)
-    user_ans = int(input("\nif you want to convert a binary number to a decimal, enter 1. \nDecimal to a binary, enter 2. \nAdd two binary numbers, enter 3. \nAnd if you want to finish, enter 4 \n"))
+    elif user_ans == 4:
+        h = input("Enter a hexadecimal number: ")
+        dec_h_n = functions_binary_and_dicimals.hexToDec(h)
+        print("The decimal number will be: ", dec_h_n)
+    user_ans = int(input("if you want to convert a binary number to a decimal, enter 1. \nDecimal to a binary, enter 2. \nAdd two binary numbers, enter 3. \nHexadecimals to decimals, enter 4. \n Decimals to hexadecimals, enter 5. \nHexadecimals to binary, enter 6. \nBinary to hexadecimals, enter 7.\n And if you want to finish, enter 8. \n "))
 print("Good bye!")
     
     
