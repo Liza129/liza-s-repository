d053ffcaf47292973a86696c7495861a808e3bec
Liza129 <144443800+Liza129@users.noreply.github.com>
2024-02-09 12:08:19 +0000

    Update functions_binary_and_dicimals.py

--- functions_binary_and_dicimals.py
+++ functions_binary_and_dicimals.py
@@ -21,7 +21,7 @@ def decimalToBi(d_0):
             remaind.append(1)
         d_0 = d_0//2
     remaind = remaind[::-1]
-    response = remaind
+    response = "".join(str(x) for x in remaind)
     return response
 
 
@@ -89,6 +89,7 @@ def hexToDec(h):
     x = 0
     mult = 0
     dec_h_n = 0
+    sum_h_d = 0
     h_0 = h[::-1]
     letters = ["A","B","C","D","E","F"]
     numbers = [10,11,12,13,14,15]
@@ -98,7 +99,84 @@ def hexToDec(h):
             i = numbers[x]
         else:
             i = int(i)
-            dec_h_n += i * 16^mult
-            mult += 1
+        sum_h_d = i * (16**mult)
+        dec_h_n += sum_h_d
+        mult += 1
     response = dec_h_n
     return response
+
+def decimalToHex(d_h):
+    hexid = []
+    while d_h != 0 :
+        modul = d_h%16
+        if modul == 0:
+            hexid.append(0)
+        else:
+            hexid.append(modul)
+        d_h = d_h // 16   
+    hexid = hexid[::-1]
+    response = "".join(str(x) for x in hexid)
+    return response
+
+def hexToBin(h_b):
+    bin_h = []
+    h_b = h_b[::-1]
+    letters = ["A","B","C","D","E","F"]
+    numbers = [10,11,12,13,14,15]
+    for i in h_b:
+        if i.isalpha():
+            if i in letters:
+                x = letters.index(i)
+                i = numbers[x]
+        else:
+            i = int(i)
+        while i != 0 :
+            modul = i
+            modul = modul%2
+            if modul == 0:
+                bin_h.append(0)
+            else:
+                bin_h.append(1)
+            i = i//2
+        while len(bin_h)%4 != 0:
+            bin_h.append(0)
+    bin_h = bin_h[::-1]
+    while bin_h[0] != 1:
+        bin_h.pop(0)
+    response = "".join(str(x) for x in bin_h)
+    return response
+     
+def binToHex(b_h):
+    b_h = list(b_h)
+    b_h = b_h[::-1]
+    dex = []
+    four_l = []
+    
+    letters = ["A","B","C","D","E","F"]
+    numbers = [10,11,12,13,14,15]
+    
+    while len(b_h) % 4 != 0:
+        b_h.append(0)
+        
+    for i in b_h:
+        four_l.append(i)
+        dex_n = 0
+        while len(four_l) % 4 == 0 and len(four_l) != 0:
+            for i in four_l:
+                dex_n = dex_n*2 + int(i)
+            four_l.clear()
+            dex.append(dex_n)
+        
+    
+    count = 0
+    for i in dex:
+        
+        if i > 9:
+                 
+            x = numbers.index(i)
+            i = letters[x]
+            dex[count] = i
+        count +=1  
+                        
+    response = "".join(str(x) for x in dex)
+ 
