652f3c82745d08833de877f017044ed6d1544bce
Liza129 <144443800+Liza129@users.noreply.github.com>
2024-04-15 11:47:32 +0100

    Add files via upload

---
+++ flowcharts.py
@@ -0,0 +1,23 @@
+# https://miro.com/app/board/uXjVKVUdqHc=/?share_link_id=129609940055
+#1
+c = int(input("Enetr temperature in Celsius: "))
+f = (9.0/5.0*c)+32
+print(f)
+#1.2
+
+f = int(input("Enter the value of f: "))
+c = (f - 32)*5/9
+print(c)
+
+#2
+s = int(input("Enter the lengh of the side: "))
+a == s**2
+p = s*4
+print("Area is: ", a, "\nPerimeter is: ",p)
+#4
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+if num1 < num2:
+    print("Smallest is num1")
+else:
+    print("Smallest is num2")
